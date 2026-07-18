61f8aa21949210cb427c6a894ed7f3bd80139b1a
kacobjerby <kerby.jacob.r@gmail.com>
2026-07-18 18:12:10 -0500

    fixed gaymode meta function

--- script.js
+++ script.js
@@ -8,6 +8,19 @@ document.getElementById("gayBtn").addEventListener("click", () => {
 });
 */
 
-function gaymode() {
+function gaymode(onOffToggle) {
+    switch (onOffToggle) {
+        case "On":
+            console.log("Turning Gay mode on");
+            document.body.classList.add("gay");
+        case "Off":
+            console.log("Turning Gay mode off");
+            document.body.classList.remove("gay");
+        case "Toggle":
+            console.log("Toggling Gay mode");
+            document.body.classList.toggle("gay");
+        default:
+            console.log("Usage: gaymode(On/Off/Toggle)")
+    }
     document.body.classList.toggle("gay");
 }
