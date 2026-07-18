1dbbf12b42fb5f5c1976553a46d6746735b00163
kacobjerby <kerby.jacob.r@gmail.com>
2026-07-18 17:44:51 -0500

    commented out gay mode until i learn more

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ document.getElementById("blackBtn").addEventListener("click", () => {
     document.body.classList.toggle("black-bg");
 });
 
+/*
 document.getElementById("gayBtn").addEventListener("click", () => {
     document.body.classList.toggle("gay");
 });
+*/
