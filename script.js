2be51693bba528a0b231a02f146a0dfe8381e964
kacobjerby <kerby.jacob.r@gmail.com>
2026-07-18 17:36:49 -0500

    Added GAY Mode

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 document.getElementById("blackBtn").addEventListener("click", () => {
     document.body.classList.toggle("black-bg");
 });
+
+document.getElementById("gayBtn").addEventListener("click", () => {
+    document.body.classList.toggle("gay");
+});
