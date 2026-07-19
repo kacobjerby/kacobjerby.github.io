3fc8fe5da04bcdbef4626a2e03f8978e204170b6
kacobjerby <kerby.jacob.r@gmail.com>
2026-07-18 20:37:23 -0500

    Two Page Update attempt two

--- RGBscript.js
+++ RGBscript.js
@@ -2,12 +2,17 @@ document.getElementById("blackBtn").addEventListener("click", () => {
     document.body.classList.toggle("black-bg");
 });
 
-const r = document.getElementById("rValue")
-const g = document.getElementById("gValue")
-const b = document.getElementById("bValue")
-submitBtn.addEventListener('click', () => {
-    const newR = 255-r;
-    const newG = 255-g;
-    const newB = 255-b;
+const r = document.getElementById("rValue");
+const g = document.getElementById("gValue");
+const b = document.getElementById("bValue");
+
+const submitBtn = document.getElementById("submitBtn");
+const textOutput = document.getElementById("textOutput");
+
+submitBtn.addEventListener("click", () => {
+    const newR = 255 - Number(r.value);
+    const newG = 255 - Number(g.value);
+    const newB = 255 - Number(b.value);
+
     textOutput.value = `${newR}, ${newG}, ${newB}`;
 });
